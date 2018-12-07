0e052f99a5564d916e668baedcf216feec26d995
DeepLearning VM <google-dl-platform@googlegroups.com>
2018-12-07 10:27:42 +0000

    added autoencoder and low models

--- dark.py
+++ dark.py
@@ -102,14 +102,15 @@ class Darknet(nn.Module):
         return [conv_layer(ch_in, ch_in*2,stride=stride)
                ] + [(res_layer(ch_in*2)) for i in range(num_blocks)]
     
-    def __init__(self, num_blocks:Collection[int], num_classes:int, nf=32, se=False, nf_in=3):
+    def __init__(self, num_blocks:Collection[int], num_classes:int, nf=32, se=False, nf_in=3, head=True):
         "create darknet with `nf` and `num_blocks` layers"
         super().__init__()
         layers = [conv_layer(nf_in, nf, ks=3, stride=1)]
         for i,nb in enumerate(num_blocks):
             layers += self.make_group_layer(nf, nb, stride=2-(i==1), se=se)
             nf *= 2
-        layers += [nn.AdaptiveAvgPool2d(1), Flatten(), nn.Linear(nf, num_classes)]
+        
+        if head: layers += [nn.AdaptiveAvgPool2d(1), Flatten(), nn.Linear(nf, num_classes)]
         self.layers = nn.Sequential(*layers)
 
     def forward(self, x): return self.layers(x)
